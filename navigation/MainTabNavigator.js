66f014c694f5664249bbf9c50305cf6e2d60907f
Chukwuma Okere <chukwumaokere@yahoo.com>
2019-03-01 00:13:33 -0600

    cleanup file structure

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -44,7 +44,7 @@ const SettingsStack = createStackNavigator({
 });
 
 SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
+  tabBarLabel: 'Account',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
